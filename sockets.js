92e31f572e984ca4a8d5a06bfc8c6b92b928401a
Ana-teston <anapaulateston@gmail.com>
2023-04-04 12:39:35 +0200

    integrate express & nameSpaces

---
+++ sockets.js
@@ -0,0 +1,35 @@
+let readyPlayerCount = 0;
+
+function listen(io) {
+  const pongNameSpace = io.of('/pong');
+  pongNameSpace.on('connection', (socket) => {
+    console.log('a user connected', socket.id);
+  
+    socket.on('ready', () => {
+      console.log('player ready', socket.id);
+      
+      readyPlayerCount++;
+    
+      if(readyPlayerCount % 2 === 0) {
+        pongNameSpace.emit('startGame', socket.id);
+      };
+    });
+  
+    socket.on('paddleMove', (paddleData) => {
+      socket.broadcast.emit('paddleMove', paddleData);
+    });
+  
+    socket.on('ballMove', (ballData) => {
+      socket.broadcast.emit('ballMove', ballData);
+    });
+  
+    socket.on('disconnect', (reason) => {
+      console.log(`Client ${socket.id} disconnected ${reason}`)
+    });
+  });
+}
+
+
+module.exports = {
+  listen,
+}
